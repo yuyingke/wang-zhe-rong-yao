9b45e28d3f7f1a0fd732741d96eedacebbd24bb9
yuyingke <yuyingke@outlook.com>
2020-03-15 14:52:30 +0800

    Update autotap.py
    更改为小数延时，控制更加精准

--- autotap.py
+++ autotap.py
@@ -1,85 +1,73 @@
 import subprocess
 import random
 import time
-#夜神模拟器   adb connect 127.0.0.1:62001
-#天天模拟器   adb connect 127.0.0.1:6555
-#夜神模拟器
+
 #模拟点击坐标，加上随机数避免每次点击同一点
 def tap(x, y):
-    subprocess.call('adb shell input tap {} {}'.format(x + random.randint(0,8), y + random.randint(0,8)),shell = True)
+    subprocess.call('adb shell input tap {} {}'.format(x + random.randint(0,5), y + random.randint(0,5)),shell = True)
     
 #最后一关大师难度(鲁班周瑜芈月)，可自行换英雄
 def start(i):
-
-    print('冒险之旅')
-    tap(745, 395)
-    s1 = 1 + random.randint(0,1)
-    print('等待' , s1 , '秒')
-    time.sleep( s1 )
-
-    print('冒险模式')
-    tap(575, 255)
-    s2 = 1 + random.randint(0,1)
-    print('等待' , s2 , '秒')
-    time.sleep( s2 )
-
-    print('选择挑战')
-    tap(475, 265)
-    s3 = 1 + random.randint(0,1)
-    print('等待' , s3 , '秒')
-    time.sleep( s3 )
-    
+ 
     print('下一步')
-    tap(745, 460)
-    t1 = 2 + random.randint(0,1)
+    #tap(1670, 920)
+    tap(1850, 990)
+    t1 = 0.5 + round(random.uniform(0,0.5),1)
     print('等待' , t1 , '秒')
     time.sleep( t1 )
 
     print('点击闯关')
-    tap(720, 438)
+    tap(1600, 870)
     
-    t2 = 15 + random.randint(0,1)
+    t2 = 11.6 + round(random.uniform(0,0.5),1)
     print('加载界面，' , '等待' , t2 , '秒')
     time.sleep( t2 )
     
     print('跳过开始对话')
-    tap(920, 20)
-    t3 = 64 + random.randint(0,1)
+    tap(2070, 50)
+    t3 = 63 + round(random.uniform(0,0.3),1)
     print('等待' , t3 , '秒')
     time.sleep( t3 )
     
     print('跳过墨子对话')
-    tap(920, 20)
-    t4 = 24 + random.randint(0,1)
+    tap(2070, 50)
+    t4 = 21 + round(random.uniform(0,0.2),1)
     print('等待' , t4 , '秒')
     time.sleep( t4 )
     
     print('跳过白起对话')
-    tap(920, 20)
-    t5 = 35 + random.randint(0,1)
+    tap(2070, 50)
+    t5 = 36.3 + round(random.uniform(0,0.5),1)
     print('等待' , t5 , '秒')
     time.sleep( t5 )
     
     print('跳过最后对话')
-    tap(920, 20)
+    tap(2070, 50)
 
-    t6 = 12 + random.randint(0,1)
+    t6 = 9 + round(random.uniform(0,0.5),1)
     print('等待结束' , '等待' , t6 , '秒')
     time.sleep(t6)
 
-    print('完成' , (i+1) , '次,耗时：' , (s1+s2+s3+t1+t2+t3+t4+t5+t6) , '\n')
+    print('点击任意位置')
+    tap(1850, 990)
+    #time.sleep(0.5)
+    #tap(1820, 890)
+    t7 = 4 + round(random.uniform(0,0.5),1)
+    print('等待' , t7 , '秒')
+    time.sleep(t7)
+
+    print('完成' , (i+1) , '次,耗时：' , (t1+t2+t3+t4+t5+t6+t7) , '\n')
 
 if __name__ == "__main__":    
     for i in range(12):
         start(i)
     time.sleep(5)
-
-    for i in range(12):
-        start(i)
-    time.sleep(5)
     
-    for i in range(12):
+    for i in range(14):
         start(i)
     time.sleep(5)
-      
+  
+    for i in range(14):
+        start(i)
+    time.sleep(5)
     pass
